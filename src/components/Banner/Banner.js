0618ea7d68eabf49d319fcabf167ac5b488e739e
Natanna99 <erzafairy79@gmail.com>
2025-01-26 00:50:28 -0300

    first commit

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,10 @@
+import React from "react";
+import style from "./Banner.module.css";
+
+export function Banner() {
+  return (
+    <div className={style.banner}>
+      <img src="/image/organo.png" alt="Banner" />
+    </div>
+  );
+}
