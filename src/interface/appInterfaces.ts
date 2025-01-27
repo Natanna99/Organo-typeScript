8cd18dee86438a6be08aefaf8676c80c834735b9
Natanna99 <erzafairy79@gmail.com>
2025-01-27 11:56:35 -0300

    Feature: arquivo de interfaces globais

---
+++ src/interface/appInterfaces.ts
@@ -0,0 +1,13 @@
+export interface ListTimeInterface {
+  id: string;
+  time: string;
+  color: string;
+}
+
+export interface ListCollaboratorsInterface {
+  id: string;
+  name: string;
+  image: string;
+  role: string;
+  fav?: boolean;
+}
