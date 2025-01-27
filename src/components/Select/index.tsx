ca15e0588e34b4725feeca31b7f5528d6a4b99e7
Natanna99 <erzafairy79@gmail.com>
2025-01-27 14:55:54 -0300

    Chore: componente de select para ts

--- src/components/Select/index.tsx
+++ src/components/Select/index.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 import style from "./Select.module.css";
+import { ListTimeInterface } from "./../../interface/appInterfaces";
 
-export function Select({ label, options, setValue, value }) {
+interface SelectInterface {
+  label: string;
+  options: ListTimeInterface[];
+  setValue: (event: React.ChangeEvent<HTMLSelectElement>) => void;
+  value: string;
+}
+
+export function Select({ label, options, setValue, value }: SelectInterface) {
   return (
     <div className={style.grupSelect}>
       <label>{label}</label>
-      <select mandatory onChange={setValue} value={value}>
+      <select required onChange={setValue} value={value}>
         <option value="">Selecione</option>
         {options?.map((option) => (
           <option key={option.value} value={option.value}>
